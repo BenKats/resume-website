dfa7f517937162406dabe9602220e6ae852896cc
BenKats <katsbenjamin@gmail.com>
2019-10-25 10:28:09 -0400

    Sanitized CSS and commented out uneeded code

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
-            <header className='App-header'>
-                <img src={logo} className='App-logo' alt='logo' />
-                <h1> <span role="img" aria-label="Construction Worker Emoji">&#128119;
-</span>  This website is still a work in progress <span role="img" aria-label="Construction Crane Emoji">&#127959;</span></h1>
-                <p>But you're here, consider following me on my social medias :)</p>
-
-                <div>
-                    <ul>
-                        <li>
-                            <a href='https://linkedin.com/in/benkats'>LinkedIn</a>
-                        </li>
-                        <li>
-                            <a href='https://www.instagram.com/ben_kats/'>Instagram</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <a
-                    className='App-link'
-                    href='https://reactjs.org'
-                    target='_blank'
-                    rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
+            <h1>Welcome One and All</h1>
         </div>
     );
 }
 
 export default App;
+// Initial Base Layout without project section
+// <NavBar></NavBar>
+// <WelcomeBanner></WelcomeBanner>
+// <AboutMe></AboutMe>
+// <ContactMe></ContactMe>
+// <Footer></Footer>
